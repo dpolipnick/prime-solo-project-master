ff76a01b5854e304a2396b6de3582ddcce563975
dpolipnick <dick@onlinegrowthsystems.com>
2018-11-21 20:22:13 -0600

    fixed SQL GET habits code

--- server/routes/habits.router.js
+++ server/routes/habits.router.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 // This will retrieve the habits from the DB
 router.get('/', (req, res) => {
-    const queryText = `SELECT * FROM "habits"
-    JOIN "categories" ON "categories"."id"="habits"."category_id";`;
+    const queryText = `SELECT "habits".*, "categories"."category" FROM "habits"
+    JOIN "categories" ON "habits"."category_id" = "categories"."id";`;
     pool.query(queryText)
       .then((result) => { res.send(result.rows); })
       .catch((error) => {
--- src/components/HabitsItem/HabitsItem.js
+++ src/components/HabitsItem/HabitsItem.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import React, { Component } from 'react';
 // Styles
 import { Delete, Create} from '@material-ui/icons';
+import swal from 'sweetalert';
 
 class HabitsItem extends Component {
 
@@ -13,10 +14,26 @@ class HabitsItem extends Component {
         return (
             <div>
                 <h3>{habit.habit}</h3>
+                <h4>{habit.id}</h4>
                 <p className="text-black">Category: {habit.category}</p>
-                <Delete onClick={() => {this.props.dispatch(
-                                        {type: 'DELETE_HABIT', payload: habit}
-                                        )}}/>
+                <Delete onClick={() => { 
+                    swal({
+                        title: "Are you sure?",
+                        text: "Once deleted, you will not be able to recover this habit!",
+                        icon: "warning",
+                        buttons: true,
+                        dangerMode: true,
+                    })
+                    .then((willDelete) => {
+                        if (willDelete) {
+                        swal("Awesome! You conquered your bad habit and it's been deleted!", {
+                            icon: "success",
+                        });
+                        {this.props.dispatch(
+                            {type: 'DELETE_HABIT', payload: habit}
+                        )}}
+                    })
+                }}/>
                 <Create />
             </div>
         );
