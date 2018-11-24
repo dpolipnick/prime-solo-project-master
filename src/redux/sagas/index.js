a405346d1da039740b974c895b1a80af42a58c06
dpolipnick <dick@onlinegrowthsystems.com>
2018-11-24 17:54:23 -0600

    added watcher to rootSaga

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -4,6 +4,7 @@ import registrationSaga from './registrationSaga';
 import userSaga from './userSaga';
 import categoriesSaga from './categoriesSaga';
 import habitsSaga from './habitsSaga';
+import occurrenceSaga from './occurrenceSaga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -19,5 +20,6 @@ export default function* rootSaga() {
     userSaga(),
     categoriesSaga(),
     habitsSaga(),
+    occurrenceSaga(),
   ]);
 }
