8ba430074998d6763f5b8cd0366a20dbaa284a2d
dpolipnick <dick@onlinegrowthsystems.com>
2018-11-30 15:55:07 -0600

    started about page and tips page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -7,11 +7,26 @@ import React from 'react';
 
 const AboutPage = () => (
   <div>
-    <div>
-      <p>
-        This about page is for anyone to read!
-      </p>
+    <header className="homeHeader">
+      <br/>
+      <img alt="" className="nav-title" src={require('./logo.png')}/>
+      <br/>
+      <h2>An interactive way to be who you want to be</h2>
+      <br/>
+    </header>
+    <h2>Testimonial</h2>
+    <img alt="" className="testimonialPic" src={require('./before.png')}/>
+    <div className="testimonial">
+      <p>"I had a really hard time trying to stop swearing... Habit Breaker&trade; changed my life! I owe it all to you!"</p>
+      <p className="signature">-Trevor</p>
     </div>
+    <img alt="" className="testimonialPic" src={require('./after.png')}/>
+    <br/>
+    <h2 className="funColors">Our Mission</h2>
+    <p>We developed Habit Breaker&trade; to help the helpless. 
+      To bring happiness to those who are in sorrow. 
+      To be a beacon of light and empower those trapped in darkness 
+      to achieve their destiny. We change lives.</p>
   </div>
 );
 
---
+++ src/components/Tips/Tips.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Tips = () => (
+  <div>
+    <header className="homeHeader">
+      <br/>
+      <img alt="" className="nav-title" src={require('./logo.png')}/>
+      <br/>
+      <h2>An interactive way to be who you want to be</h2>
+      <br/>
+    </header>
+    <h2>Tips</h2>
+    <p>We have done extensive research </p>
+  </div>
+);
+
+export default Tips;
